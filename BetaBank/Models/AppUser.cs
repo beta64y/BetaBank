029dcd306691f916d47ec11bd1557161b3e82bfe
Yəhya Camalzadə <camalzadeyahya8@gmail.com>
2024-06-12 19:20:21 +0400

    App User model added

---
+++ BetaBank/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Principal;
+
+namespace BetaBank.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
