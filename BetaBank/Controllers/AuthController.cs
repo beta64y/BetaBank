08d6a14235fea5cbeeec95bb96caf156a09e2234
Yəhya Camalzadə <camalzadeyahya8@gmail.com>
2024-06-12 12:53:33 +0400

    auth section added

---
+++ BetaBank/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BetaBank.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            //if (User.Identity.IsAuthenticated)
+            //{
+            //    return RedirectToAction("Index", "Home");
+            //}
+            return View();
+        }
+    }
+}
